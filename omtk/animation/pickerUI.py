b5a2a6e2a37ef57da7128293c457d6ccb08c047d
Renaud Lessard Larouche <sigmao@gmail.com>
2014-10-15 20:31:22 -0400

    add wip ui for namespace, command and grid

--- omtk/animation/pickerUI.py
+++ omtk/animation/pickerUI.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/Users/renaudlessardlarouche/GitHub/omtk/omtk/animation/pickerUI.ui'
 #
-# Created: Mon Oct 13 22:53:27 2014
+# Created: Wed Oct 15 20:29:39 2014
 #      by: pyside-uic 0.2.14 running on PySide 1.2.0
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,12 +12,51 @@ from PySide import QtCore, QtGui
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(567, 600)
+        MainWindow.resize(423, 547)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.verticalLayout = QtGui.QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName("verticalLayout")
+        self.groupBox = QtGui.QGroupBox(self.centralwidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.groupBox.sizePolicy().hasHeightForWidth())
+        self.groupBox.setSizePolicy(sizePolicy)
+        self.groupBox.setMinimumSize(QtCore.QSize(0, 0))
+        self.groupBox.setMaximumSize(QtCore.QSize(16777215, 140))
+        self.groupBox.setObjectName("groupBox")
+        self.verticalLayout_2 = QtGui.QVBoxLayout(self.groupBox)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtGui.QLabel(self.groupBox)
+        self.label.setMaximumSize(QtCore.QSize(16777215, 20))
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.lineEdit_namespace = QtGui.QLineEdit(self.groupBox)
+        self.lineEdit_namespace.setObjectName("lineEdit_namespace")
+        self.horizontalLayout.addWidget(self.lineEdit_namespace)
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+        self.checkBox = QtGui.QCheckBox(self.groupBox)
+        self.checkBox.setObjectName("checkBox")
+        self.verticalLayout_2.addWidget(self.checkBox)
+        self.horizontalLayout_2 = QtGui.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.label_command = QtGui.QLabel(self.groupBox)
+        self.label_command.setObjectName("label_command")
+        self.horizontalLayout_2.addWidget(self.label_command)
+        self.lineEdit_command = QtGui.QLineEdit(self.groupBox)
+        self.lineEdit_command.setObjectName("lineEdit_command")
+        self.horizontalLayout_2.addWidget(self.lineEdit_command)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
+        self.verticalLayout.addWidget(self.groupBox)
         self.widget = QtGui.QWidget(self.centralwidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.widget.sizePolicy().hasHeightForWidth())
+        self.widget.setSizePolicy(sizePolicy)
         self.widget.setObjectName("widget")
         self.verticalLayout.addWidget(self.widget)
         MainWindow.setCentralWidget(self.centralwidget)
@@ -39,6 +78,10 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "Squeeze Picker (prototype)", None, QtGui.QApplication.UnicodeUTF8))
+        self.groupBox.setTitle(QtGui.QApplication.translate("MainWindow", "Settings", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("MainWindow", "Namespace:", None, QtGui.QApplication.UnicodeUTF8))
+        self.checkBox.setText(QtGui.QApplication.translate("MainWindow", "Enable Grid", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_command.setText(QtGui.QApplication.translate("MainWindow", "Command:", None, QtGui.QApplication.UnicodeUTF8))
         self.toolBar.setWindowTitle(QtGui.QApplication.translate("MainWindow", "toolBar", None, QtGui.QApplication.UnicodeUTF8))
         self.actionImport.setText(QtGui.QApplication.translate("MainWindow", "Import", None, QtGui.QApplication.UnicodeUTF8))
         self.actionExport.setText(QtGui.QApplication.translate("MainWindow", "Export", None, QtGui.QApplication.UnicodeUTF8))

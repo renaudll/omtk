631249f1bc4fbbb6827a79826390c8f8290ccf78
Renaud Lessard Larouche <sigmao@gmail.com>
2014-10-17 00:54:12 -0400

    implement autoCreate, load_layout, save_layout picker now come with defaultLayout and background

--- omtk/animation/picker.py
+++ omtk/animation/picker.py
@@ -1,19 +1,21 @@
 from omtk.libs.libQt import QtGui, QtCore, getMayaWindow
 from omtk.libs import libSerialization
 import logging, os
+from maya import cmds
+import pymel.core as pymel
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
 class HitBox(object):
-    _default_width = 42
-    _default_height = 42
+    _default_width = 32
+    _default_height = 32
     _default_posx = 0
     _default_posy = 0
 
     _color_selected = QtGui.QColor(128,128,128,128)
     _color_unselected = QtGui.QColor(0, 0, 0, 128)
 
-    _dagpath = None
     _command_language = 'python'
 
     def __init__(self, **kwargs):
@@ -22,41 +24,40 @@ class HitBox(object):
         self.posx = HitBox._default_posx # todo: remove
         self.posy = HitBox._default_posy # todo: remove
         self.label = 'debug'
+        self.command = None
         self.__dict__.update(kwargs)
+
         self._hitbox = QtCore.QRect(self.posx, self.posy, self.width, self.height)
-        self.need_update = True
-        self.is_selected = False
+        self._need_update = True
+        self._selected = False
 
     @property
     def selected(self):
-        return self.is_selected
+        return self._selected
 
     @selected.setter
     def selected(self, val):
-        if self.is_selected != val:
-            self.is_selected = val
-            self.need_update = True
+        if self._selected != val:
+            self._selected = val
+            self._need_update = True
 
     # todo: convert to property
     def set_position(self, newpos):
         if not isinstance(newpos, QtCore.QPoint): raise IOError
         self.posx = newpos.x()
         self.posy = newpos.y()
-        self._hitbox.moveTo(QtCore.QPoint(self.posx, self.posy))
 
     def draw(self, qp):
-        #if self.need_update is True:
+        self._hitbox = QtCore.QRect(self.posx, self.posy, self.width, self.height)
         qp.drawRect(self._hitbox)
         qp.fillRect(self._hitbox, HitBox._color_selected if self.selected else HitBox._color_unselected)
         qp.drawText(self._hitbox, QtCore.Qt.AlignCenter, self.label)
-        #self.need_update = False
 
     def execute(self):
         if self._command_language == 'python':
-            if self._dagpath:
-                from maya import cmds
-                if cmds.objExists(self._dagpath):
-                    cmds.select(self._dagpath)
+            if self.command:
+                if cmds.objExists(self.command):
+                    cmds.select(self.command)
         else:
             raise NotImplementedError('Invalid command_language: {0}'.format(self._command_language))
 
@@ -65,11 +66,9 @@ class HitBox(object):
 
 class PickerCore(object):
     def __init__(self):
-        self.hitboxes = [
-            HitBox(posx=20),
-            HitBox(posx=80),
-            HitBox(posx=140)
-        ]
+        self.hitboxes = []
+        self.path_backgorund = os.path.join(current_dir, "layout.jpg")
+        self._qBackground = QtGui.QImage(self.path_backgorund)
 
     def select(self, items, more=False):
         # Clear if the user miss
@@ -84,7 +83,7 @@ class PickerCore(object):
             for hitbox in self.hitboxes:
                 hitbox.selected = False
         for hitbox in items:
-            hitbox.is_selected = True
+            hitbox._selected = True
 
 
     def draw(self, qt):
@@ -124,6 +123,18 @@ class PickerCore(object):
                     hitbox.y() + y
                 )
 
+    def get_free_slot(self, w=HitBox._default_width, h=HitBox._default_height):
+        rect_test = QtCore.QRect(0, 0, w, h)
+        canvas_w = self._qBackground.width()-w
+        canvas_h = self._qBackground.height()-h
+        for y in xrange(0, canvas_h, h):
+            for x in xrange(0, canvas_w, w):
+                pnt = QtCore.QPoint(x, y) # todo: optimise
+                rect_test.moveTo(pnt)
+                if len(self.hitboxes_from_qrect(rect_test)) == 0:
+                    return pnt
+        return QtCore.QPoint(0,0)
+
 class EmptyState(object):
     @classmethod
     def EnterState(cls, *args):
@@ -158,7 +169,7 @@ class State_MouseSelect(EmptyState):
 
         # Proceed with selection
         for h in cls.ui.core.hitboxes:
-            if h.is_selected:
+            if h._selected:
                 h.execute()
 
         cls.ui.selectionChanged.emit()
@@ -170,7 +181,7 @@ class State_MouseDrag(EmptyState):
         cls.ui = ui
         cls.dragged_items = {}
         for h in cls.ui.core.hitboxes:
-            if h.is_selected:
+            if h._selected:
                 cls.dragged_items[h] = h._hitbox.topLeft()
         cls.old_mouse_pos = cls.ui.mouse_pos
     @classmethod
@@ -212,12 +223,15 @@ class PickerWidget(QtGui.QWidget):
 
     def _get_selection_rect(self):
         mouse_pos = self.mapFromGlobal(QtGui.QCursor.pos())
-        return_val = QtCore.QRect(self._selection_start, mouse_pos)
+        return_val = QtCore.QRect(self.state.old_mouse_pos, mouse_pos)
         return return_val
 
     def paintEvent(self, event):
         qp = QtGui.QPainter()
         qp.begin(self)
+
+        qp.drawImage(QtCore.QPoint(), self.core._qBackground)
+
         qp.setPen(QtGui.QColor(168, 34, 3))
         qp.setFont(QtGui.QFont('Decorative', 10))
         self.core.draw(qp)
@@ -234,13 +248,13 @@ class PickerWidget(QtGui.QWidget):
         y = event.y()
         log.debug('Click at {0}, {1}'.format(x, y))
 
-        self.selected_items = [h for h in self.core.hitboxes if h.is_selected]
+        self.selected_items = [h for h in self.core.hitboxes if h._selected]
         self.mouse_pos = event.pos()
         hitbox = self.core.hitboxes_from_pos(self.mouse_pos.x(), self.mouse_pos.y())
 
         if not hitbox: # If nothing is clicked on
             self.state = State_MouseSelect
-        elif hitbox.is_selected: # If the thing that is clicked on is selected
+        elif hitbox._selected: # If the thing that is clicked on is selected
             self.state = State_MouseDrag
         else:
             self.state = State_MouseSelect
@@ -276,22 +290,28 @@ import pickerUI
 reload(pickerUI)
 class PickerGUI(QtGui.QMainWindow, pickerUI.Ui_MainWindow):
     def __init__(self, parent=getMayaWindow()):
-        super(PickerGUI, self).__init__(None)
+        super(PickerGUI, self).__init__(parent, QtCore.Qt.WindowStaysOnTopHint)
         self.setupUi(self)
         self.core = PickerCore()
 
         # HACK: Replace widget by our custom widget
         self.verticalLayout.removeWidget(self.widget)
         self.widget = PickerWidget(self.core)
-        self.verticalLayout.addWidget(self.widget)
+        self.widget.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout.insertWidget(0,self.widget)
         self.update()
 
         self.actionImport.triggered.connect(self.import_)
         self.actionExport.triggered.connect(self.export)
+        self.actionCreateAuto.triggered.connect(self.createAuto)
 
         self.actionUpdateCommand.triggered.connect(self.commandChanged)
 
         self.widget.selectionChanged.connect(self.onSelectionChanged)
+
+        print self.core._qBackground.width()
+        self.widget.setFixedWidth(self.core._qBackground.width()) # autosize
+        self.widget.setFixedHeight(self.core._qBackground.height())
 
     def keyPressEvent(self, event):
         print event.key()
@@ -335,17 +355,28 @@ class PickerGUI(QtGui.QMainWindow, pickerUI.Ui_MainWindow):
         except Exception, e:
             log.exception(e)
 
+    def create_buttons_for_objs(self, objs):
+        for obj in objs:
+            pos = self.core.get_free_slot()
+            self.core.hitboxes.append(
+                HitBox(command=obj.__melobject__(), posx=pos.x(), posy=pos.y())
+            )
+        self.widget.update()
+
+    def createAuto(self):
+        self.create_buttons_for_objs(pymel.selected())
+
     def commandChanged(self, *args):
         val = str(self.lineEdit_command.text())
         for h in self.core.hitboxes:
-            if h.is_selected:
-                h._dagpath = val
+            if h._selected:
+                h.command = val
 
     def updateLayout(self):
-        selected_items = [h for h in self.core.hitboxes if h.is_selected]
+        selected_items = [h for h in self.core.hitboxes if h._selected]
         if len(selected_items) == 1:
             self.lineEdit_command.setEnabled(True)
-            self.lineEdit_command.setText(selected_items[0]._dagpath)
+            self.lineEdit_command.setText(selected_items[0].command)
         else:
             self.lineEdit_command.setEnabled(False)
             self.lineEdit_command.setText('')
@@ -353,10 +384,28 @@ class PickerGUI(QtGui.QMainWindow, pickerUI.Ui_MainWindow):
     def onSelectionChanged(self):
         self.updateLayout()
 
-
-
 gui = None
 def show():
     global gui
     gui = PickerGUI()
     gui.show()
+
+def load_layout(name):
+    path = os.path.join(current_dir, name + '.json')
+    if os.path.exists(path): gui.core = libSerialization.import_yaml_file(path)
+    gui.widget.core = gui.core # hack
+    gui.widget.update()
+
+def save_layout(name):
+    path = os.path.join(current_dir, name + '.json')
+    libSerialization.export_yaml_file(gui.widget.core, path)
+
+# Reload .ui
+import pysideuic
+pysideuic.compileUiDir(current_dir)
+
+'''
+from omtk.animation import picker; reload(picker)
+picker.show()
+picker.load_layout('defaultLayout')
+'''

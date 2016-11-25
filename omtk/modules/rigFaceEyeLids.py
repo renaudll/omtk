dd63b4d99a7a821308b7e2552e8fc0245f25834f
Renaud Lessard Larouche <sigmao@gmail.com>
2016-11-25 14:35:02 -0500

    rename FaceLids module to FaceEyeLids

--- omtk/modules/rigFaceEyeLids.py
+++ omtk/modules/rigFaceEyeLids.py
@@ -4,30 +4,30 @@ from omtk.libs import libCtrlShapes
 from omtk.libs import libPymel
 import pymel.core as pymel
 
-class CtrlLidUpp(rigFaceAvar.BaseCtrlFace):
+class CtrlEyeLidUpp(rigFaceAvar.BaseCtrlFace):
     def __createNode__(self, **kwargs):
         return libCtrlShapes.create_triangle_upp()
 
 
-class CtrlLidLow(rigFaceAvar.BaseCtrlFace):
+class CtrlEyeLidLow(rigFaceAvar.BaseCtrlFace):
     def __createNode__(self, **kwargs):
         return libCtrlShapes.create_triangle_low()
 
 
-class FaceLids(rigFaceAvarGrps.AvarGrpAreaOnSurface):
+class FaceEyeLids(rigFaceAvarGrps.AvarGrpAreaOnSurface):
     """
     AvarGrp setup customized for Eyelids rigging.
     FaceLids behave the same a a standard AvarGrp with an Upp and Low section.
     However to adapt ourself to non-symetrical eyes, we use two surface to slide on.
     """
-    _CLS_CTRL_UPP = CtrlLidUpp
-    _CLS_CTRL_LOW = CtrlLidLow
+    _CLS_CTRL_UPP = CtrlEyeLidUpp
+    _CLS_CTRL_LOW = CtrlEyeLidLow
     IS_SIDE_SPECIFIC=True
 
     SHOW_IN_UI = True
 
     def __init__(self, *args, **kwargs):
-        super(FaceLids, self).__init__(*args, **kwargs)
+        super(FaceEyeLids, self).__init__(*args, **kwargs)
         self.surface_upp = None
         self.surface_low = None
 
@@ -73,7 +73,7 @@ class FaceLids(rigFaceAvarGrps.AvarGrpAreaOnSurface):
             avar.surface = self.surface_low
 
     def get_default_name(self):
-        return 'Lid'
+        return 'eyelid'
 
     def get_multiplier_u(self):
         # Since the V go all around the sphere, it is too much range.
@@ -81,4 +81,4 @@ class FaceLids(rigFaceAvarGrps.AvarGrpAreaOnSurface):
         return 0.25
 
 def register_plugin():
-    return FaceLids
+    return FaceEyeLids
---
+++ omtk/patches/patch_0.3.0_convert_facelids_to_faceeyelids.py
@@ -0,0 +1,24 @@
+"""
+In omtk 0.3.0, the FaceLids module was renamed to FaceEyeLids to prevent
+confusion with the FaceLips module.
+Run this script to convert a pre-0.3.0 scene.
+"""
+
+import libSerialization
+
+cls_rename_map = (
+    ('FaceLids', 'FaceEyeLids'),
+    ('CtrlLidUpp', 'CtrlEyeLidUpp'),
+    ('CtrlLidLow', 'CtrlEyeLidLow')
+)
+affected_attr_names = ('_class', '_class_namespace')
+
+for old_name, new_name in cls_rename_map:
+    for net in libSerialization.get_networks_from_class(old_name):
+        for attr_name in affected_attr_names:
+            if not net.hasAttr(attr_name):
+                print("Missing attribute {} on {}".format(attr_name, net))
+                continue
+            print("Renaming {}".format(net))
+            attr = net.attr(attr_name)
+            attr.set('.'.join((token.replace(old_name, new_name) for token in attr.get().split('.'))))

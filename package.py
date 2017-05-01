0da09afc387808a32f043ec7687d8c3f35fa6448
Renaud Lessard Larouche <sigmao@gmail.com>
2017-05-01 19:59:24 -0400

    optimise layout

--- package.py
+++ package.py
@@ -1,8 +1,8 @@
 name = 'omtk'
 
-version = '0.4.33'
+version = '0.5.0'
 
 requires = ['libSerialization-0.1+']
 
 def commands():
-    env.PYTHONPATH.append('{root}')
+    env.PYTHONPATH.append('{root}/python')

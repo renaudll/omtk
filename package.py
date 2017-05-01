ed0eb1923b261d3ca74100f93c3221b05565a4dc
Renaud Lessard Larouche <sigmao@gmail.com>
2017-05-01 17:31:39 -0400

    better organize file structure

--- package.py
+++ package.py
@@ -5,4 +5,5 @@ version = '0.4.36'
 requires = ['libSerialization-0.1+']
 
 def commands():
-    env.PYTHONPATH.append('{root}')
+    env.PYTHONPATH.append('{root}/python')
+    env.XBMLANGPATH.append("{root}/images/%B")

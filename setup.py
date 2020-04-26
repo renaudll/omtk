64022ae8e643a6d27bac35513f9c804324e84d2c
Renaud <sigmao@gmail.com>
2020-04-25 21:17:03 -0400

    Fix tests

--- setup.py
+++ setup.py
@@ -9,9 +9,11 @@ setup(
     author="Renaud Lessard Larouche",
     author_email="sigmao@gmail.com",
     url="https://github.com/renaudll/maya-mock",
-    packages=find_packages(where="src"),
-    package_dir={"": "omtk"},
+    packages=find_packages(where="scripts"),
+    package_dir={"": "scripts"},
+    package_data={"omtk": ["compounds/*.ma"]},
     install_requires=["enum; python_version < '3.0'", "six", "mock"],
+    include_package_data=True,
     extras_require={"test": ["pytest", "pytest-cov"]},
     classifiers=[
         "Development Status :: 4 - Beta",
